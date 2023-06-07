b93489ef4b01216de45340cb64a70483dd2549aa
Klimov-T <klimoff.timofey2016@yandex.ru>
2023-06-08 01:29:01 +0300

    rotation and other added

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from math import cos, sin
 import pygame
 
 
@@ -5,7 +6,7 @@ import pygame
 
 
 
-class vector(object):
+class Vector(object):
     def __init__(self, x = 0, y = 0):
         self.Xpos = x
         self.Ypos = y
@@ -15,43 +16,52 @@ class vector(object):
     def __add__(self, other):
         self.Xpos += other.Xpos
         self.Ypos += other.Ypos
+        return self
     def __sub__(self, other):
         self.Xpos -= other.Xpos
         self.Ypos -= other.Ypos
+        return self
     def __mul__(self, other):
         self.Xpos *= other
         self.Ypos *= other
+        return self
     def __pow__(self, other):
         return (self.Xpos*other.Xpos + self.Ypos*other.Ypos)
     def __truediv__(self, other):
         self.Xpos /= other.Xpos
         self.Ypos /= other.Ypos
+        return self
     def __abs__(self):
         return ((self.Xpos**2 + self.Ypos**2)**0.5)
     def __str__(self):
         return('x = ' + str(self.Xpos) + '; y = ' + str(self.Ypos) + ';')
+    def rot(self, angle):
+        ansX = cos(angle) * self.Xpos - sin(angle) * self.Ypos
+        ansY = sin(angle) * self.Xpos + cos(angle) * self.Ypos
+        self.Xpos = ansX
+        self.Ypos = ansY
 
-class Object(object):
+class PhysObject(object):
     def __init__(self):
-        self.position = vector()
-        self.velocity = vector()
-        self.acceleration = vector()
+        self.position = Vector()
+        self.velocity = Vector()
+        self.acceleration = Vector()
         self.anglePos = 0
         self.angleVel = 0
         self.angleAcc = 0
         self.colisionR = 0
-    def setPos(self, pos = vector(), angle = 0):
+    def setPos(self, pos = Vector(), angle = 0):
         self.position = pos
         self.anglePos = angle
-    def setVel(self, vel = vector(), angle = 0):
+    def setVel(self, vel = Vector(), angle = 0):
         self.velocity = vel
         self.angleVel = angle
-    def setAcc(self, acc = vector(), angle = 0):
+    def setAcc(self, acc = Vector(), angle = 0):
         self.acceleration = acc
         self.angleAcc = angle
     def setR(self, R = 0):
         self.colisionR = R
-    def dPos(self, dPosition = vector(), dA = 0):
+    def dPos(self, dPosition = Vector(), dA = 0):
         self.position += dPosition
         self.anglePos += dA
     def dT(self, dt = 0):
